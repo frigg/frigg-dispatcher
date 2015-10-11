76dd562d86b08604bd935ad28ff1242da1d5642a
Rolf Erik Lekang <me@rolflekang.com>
2015-10-11 16:25:34 +0200

    Add loading of config from secrets file

--- src/config.js
+++ src/config.js
@@ -1,13 +1,21 @@
-export const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
-export const REDIS_PORT = process.env.REDIS_PORT || 6379;
-export const REDIS_DB = process.env.REDIS_DB || 2;
+let secrets = {};
 
-export const FRIGG_WORKER_TOKEN = process.env.FRIGG_WORKER_TOKEN;
+if (process.env.SECRETS_PATH) {
+  secrets = require(process.env.SECRETS_PATH);
+}
+
+export const REDIS_HOST = secrets.REDIS_HOST || '127.0.0.1';
+export const REDIS_PORT = secrets.REDIS_PORT || 6379;
+export const REDIS_DB = secrets.REDIS_DB || 2;
+
+export const RAVEN_DSN = secrets.RAVEN_DSN || 2;
+
+export const STATSD_HOST = secrets.STATSD_HOST;
+
+export const FRIGG_WORKER_TOKEN = secrets.FRIGG_WORKER_TOKEN;
 
 export const VERSIONS = {
-  'frigg-worker': process.env.FRIGG_WORKER_VERSION,
-  'frigg-settings': process.env.FRIGG_SETTINGS_VERSION,
-  'frigg-coverage': process.env.FRIGG_COVERAGE_VERSION,
+  'frigg-worker': secrets.FRIGG_WORKER_VERSION,
+  'frigg-settings': secrets.FRIGG_SETTINGS_VERSION,
+  'frigg-coverage': secrets.FRIGG_COVERAGE_VERSION,
 };
-
-export const STATSD_HOST = process.env.STATSD_HOST;
